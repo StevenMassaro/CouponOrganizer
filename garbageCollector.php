8dd341b55b18800923f24af8d2a6bd5b77f38e6a
Steven Massaro <steven.massaro.web@gmail.com>
2017-07-05 11:34:05 -0400

    updated garbageCollector to fix date logic
    the new code will erase coupons whose year and month match the current year and month, but whose exp day is before the current day

--- garbageCollector.php
+++ garbageCollector.php
@@ -6,27 +6,32 @@ $result = deleteExpiredCoupons($files, $dir, $todayDate);
 echo $result;
 
 function deleteExpiredCoupons($files, $dir, $todayDate){
+	$numDeleted = 0;
 	for($i = 2; $i < count($files); $i++){
 		$str = explode("_", $files[$i]);
 		$delete = isCouponExpired($todayDate, $str[1]);	
 		
 		if($delete){
-			$tmp += unlink($dir . "$str[0]_$str[1]_$str[2]_$str[3]_$str[4]");
+			$tmp = unlink($dir . "$str[0]_$str[1]_$str[2]_$str[3]_$str[4]");
+			if($tmp = TRUE){
+				$numDeleted++;
+			}
 		}
 	}
-	if($tmp == null){
-		return 0;
-	}
-	else{
-		return $tmp;
-	}
+	return $numDeleted;
 }
 
 function isCouponExpired($todayDate, $fileDate){
 	$_todayDate = explode("-", $todayDate);
 	$_fileDate = explode("-", $fileDate);
-		
-	if($_todayDate[0] >= $_fileDate[0] && $_todayDate[1] >= $_fileDate[1] && $_todayDate[2] > $_fileDate[2]){
+	
+	if($_todayDate[0] > $_fileDate[0]){
+		return true;
+	}
+	else if($_todayDate[0] == $_fileDate[0] && $_todayDate[1] > $_fileDate[1]){
+		return true;
+	}
+	else if($_todayDate[0] == $_fileDate[0] && $_todayDate[1] == $_fileDate[1] && $_todayDate[2] > $_fileDate[2]){
 		return true;
 	}
 	else{
