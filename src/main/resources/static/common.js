ec40d677ffdaafadd459108d20ba4507604cd651
Steven Massaro <steven.massaro.web@gmail.com>
2018-11-29 16:52:36 -0500

    fix UI endpoint discovery when hosting through nginx

--- src/main/resources/static/common.js
+++ src/main/resources/static/common.js
@@ -1,10 +1,22 @@
 function buildLoadPromise (requestType, url){
     return new Promise((resolve, reject) => {
     	const xhr = new XMLHttpRequest();
-    	xhr.open(requestType, window.location.origin + url);
-//    	xhr.open("GET", window.location.origin + "/list");
+    	xhr.open(requestType, getApiBaseUrl(url));
     	xhr.onload = () => resolve(xhr.responseText);
     	xhr.onerror = () => reject(xhr.statusText);
     	xhr.send();
     });
+}
+
+function getApiBaseUrl(url){
+    let pathname = window.location.pathname;
+    let baseName = "";
+    if(pathname.includes("/")){
+        const lastSlash = pathname.lastIndexOf("/");
+        baseName = pathname.substring(0, lastSlash);
+    }
+    if(!url.startsWith("/")){
+        url = "/" + url;
+    }
+    return window.location.origin + baseName + url;
 }
