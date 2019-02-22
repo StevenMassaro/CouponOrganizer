258c7e8ca9d02c68b8f251dba311a893d1107d5d
Steven Massaro <steven.massaro.web@gmail.com>
2019-02-22 15:48:30 -0500

    remove commented code

--- src/main/java/CouponOrganizer/App.java
+++ src/main/java/CouponOrganizer/App.java
@@ -6,19 +6,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
 public class App extends SpringBootServletInitializer {
-//public class App implements CommandLineRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
-
-//	final private CouponMapper cityMapper;
-//
-//	public App(CouponMapper cityMapper) {
-//		this.cityMapper = cityMapper;
-//	}
-//
-//	@Override
-//	public void run(String... args) throws Exception {
-//		System.out.println(this.cityMapper.list());
-//	}
 }
